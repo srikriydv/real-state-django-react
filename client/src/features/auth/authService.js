c9e1660dda3e3031d970f002fcd710b7a45c74ad
srikriydv <srikri5462ydv@gmail.com>
2024-11-08 08:56:49 +0545

    auth setup and redux store setup

---
+++ client/src/features/auth/authService.js
@@ -0,0 +1,48 @@
+import api from "../../utils/api";
+
+const REGISTER_URL = "/api/v1/auth/users/";
+const LOGIN_URL = "/api/v1/auth/jwt/create/";
+const ACTIVATE_URL = "/api/v1/auth/users/activation/";
+
+// Register user
+const register = async (userData) => {
+	const config = {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+
+	const response = await api.post(REGISTER_URL, userData, config);
+	return response.data;
+};
+
+// Login user
+
+const login = async (userData) => {
+	const config = {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+	const response = await api.post(LOGIN_URL, userData, config);
+	if (response.data) {
+		localStorage.setItem("user", JSON.stringify(response.data));
+	}
+	return response.data;
+};
+
+const logout = () => localStorage.removeItem("user");
+
+const activate = async (userData) => {
+	const config = {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	};
+	const response = await api.post(ACTIVATE_URL, userData, config);
+	return response.data;
+};
+
+const authService = { register, login, logout, activate };
+
+export default authService;
