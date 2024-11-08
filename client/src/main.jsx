7c3f666a31411b8e5259136dfa4e8fb9ba11ef69
srikriydv <srikri5462ydv@gmail.com>
2024-11-08 09:37:05 +0545

    HomePage setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
+  </StrictMode>
+);
